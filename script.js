f74bd7dbe9837a292591299a862ccdbc853620b3
Tung Lam <tunglam123ik54@gmail.com>
2022-10-17 13:36:17 +0700

    add moving slider

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ const modalCloseBottom = document.querySelector(".modal-close-bottom");
 const openNavBtn = document.querySelector(".open-nav-btn");
 const header = document.querySelector("#header");
 const aLinkNavs = document.querySelectorAll("#nav a");
-
+const modal_container=document.querySelector(".modal-container")
+const slider=document.querySelector("#slider");
+const slider_text_location=document.querySelector("#slider .text-location");
+const slider_text_description=document.querySelector("#slider .text-description");
 // handle modal
 for (const buyTicketBtn of buyTicketBtns) {
   buyTicketBtn.onclick = function () {
@@ -18,13 +21,12 @@ modalClose.onclick = function () {
 modalCloseBottom.onclick = function () {
   modal.classList.remove("open");
 };
-modal.onclick = function (e) {
-  if (e.target.closest(".modal-container")) {
-  } else {
+modal.onclick = function () {
     modal.classList.remove("open");
-  }
 };
-
+modal_container.onclick=function(e) {
+  e.stopPropagation();
+}
 // open and hide header,subnav
 const headerHeight = header.clientHeight;
 openNavBtn.onclick = function () {
@@ -43,3 +45,39 @@ aLinkNavs.forEach((aLinkNav) => {
     };
   }
 });
+
+// handle slider
+var sliderInfos=[
+  {
+    img:"./assets/img/slide_image_1.jpg",
+    location:"Los Angeles",
+    description:"We had the best time playing at Venice Beach!"
+  },
+  {
+    img:"./assets/img/slide_image_2.jpg",
+    location:"New York",
+    description:"The atmosphere in New York is lorem ipsum."
+  },
+  {
+    img:"./assets/img/slide_image_3.jpg",
+    location:"Chicago",
+    description:"Thank you, Chicago - A night we won't forget."
+  }
+]
+function handleSlider(){
+  let currentIndex=0;
+  setInterval(function() {
+    if(currentIndex==sliderInfos.length){
+      console.log("Hi")
+      currentIndex=0
+    }
+    console.log(currentIndex);
+    slider.style.backgroundImage=`url(${sliderInfos[currentIndex].img})`
+    slider_text_location.innerText=sliderInfos[currentIndex].location
+    slider_text_description.innerText=sliderInfos[currentIndex].description
+    currentIndex++
+    console.log(currentIndex)
+  },4000)
+}
+handleSlider()
+
